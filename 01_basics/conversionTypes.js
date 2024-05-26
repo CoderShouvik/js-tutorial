7c4bcc49a02d1e5b613f37a1e8ccd31f7b6607cb
Shouvik_Sarkar <87439972+CoderShouvik@users.noreply.github.com>
2024-05-26 09:14:30 +0000

    dataType Conversion

---
+++ 01_basics/conversionTypes.js
@@ -0,0 +1,34 @@
+
+let str = '1';
+let boolValue = Boolean(str);
+
+console.log(boolValue);
+console.log(typeof boolValue);
+
+
+
+/* Number to String */
+
+//nums = 10 (number) => '10' (string)
+
+
+/* String to Number */
+
+//str = '10' (string) => 10 (number)
+//str = 'Shouvik' or '123abc' (string) => NaN (number)  //This might create confusion
+
+/* Number to Boolean */
+
+//nums = 0 (number) => false (boolean)
+//nums = 1 or any other +ve or -ve number (number) => true (boolean)
+
+/* Boolean to Number */
+
+//boolValue = false (boolean) => 0 (number)
+//boolValue = true (boolean) => 1 (number)
+
+
+/* String to Boolean */
+
+//str = '' (string) => false (boolean)
+//str = 'any value' (string) => true (boolean)
