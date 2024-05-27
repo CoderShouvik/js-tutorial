936df09b6194df8a3f56ca790fe5e13fdc34d8fa
Shouvik_Sarkar <87439972+CoderShouvik@users.noreply.github.com>
2024-05-27 07:02:54 +0000

    added types of data

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -1,3 +1,19 @@
 console.log(typeof null);  //object
 console.log(typeof undefined);  //undefined
 console.log(typeof NaN);  //number
+
+
+/*
+        Typeof               Value
+
+        Undefined           Undefined
+        Null                Object
+        Boolean             Boolean
+        Number              Number
+        String              String
+        Array               Object
+        Object              Object
+        Function            Function
+        Symbol              Symbol
+
+*/
