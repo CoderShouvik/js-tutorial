0b77099626efffe09ff7f0bc61ff438c853a6c02
Shouvik_Sarkar <87439972+CoderShouvik@users.noreply.github.com>
2024-05-28 18:21:35 +0000

    all about numbers

---
+++ 01_basics/numbers.js
@@ -0,0 +1,39 @@
+let nums = 400;
+let balance = new Number(100);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+
+let num2 = 23.8996;
+console.log(num2.toPrecision(5))
+
+
+let num3 = 1000000;
+console.log(num3.toLocaleString('en-In'))
+
+
+/*
+    let balance = new Number(100);
+
+    balance.toString()                           //then we can use many string methods like balance.toString().length
+
+
+    let num2 = 23.8996;
+
+    num2.toPrecision(5)
+
+    num2.toFixed(2)                    23.89     //after decimal shows number of digits given input
+
+
+    let num3 = 1000000;
+
+    num3.toLocaleString()               1,000,000
+    
+    num3.toLocaleString('en-In')        10,00,000
+
+
+
+
+
+*/
