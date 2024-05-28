c3d126d7fe283c12500600d21554ebf2f1662aa2
Shouvik_Sarkar <87439972+CoderShouvik@users.noreply.github.com>
2024-05-28 19:30:00 +0000

    everything about Maths

--- 01_basics/numbers.js
+++ 01_basics/numbers.js
@@ -32,8 +32,63 @@ console.log(num3.toLocaleString('en-In'))
     
     num3.toLocaleString('en-In')        10,00,000
 
+    # in browser #
 
+    Number.   (then various math methods)
 
 
 
 */
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//  Math
+
+/* 
+
+Math.sqrt(25)                       ===>  5                                         //square root of a number
+Math.pow(5,2)                       ===>  25                                        //power of a number
+Math.round(2.4)                     ===>  2                                         //round of a number after decimal if less than 5 then floor else ceil number
+Math.floor(2.4)                     ===>  2                                         //basically the decimal value
+Math.ceil(2.4)                      ===>  3                                         //next integer
+Math.abs(-4)                        ===>  4                                         //positive value of any number
+Math.min(1,2,6,4,3)                 ===>  1                                         //smallest number of a list
+Math.max(1,2,6,4,3)                 ===>  6                                         //biggest number of a list
+Math.random()                       ===>  random value from 0to 1                       
+
+
+// very very important
+// let min = 1;
+// let max = 6;
+
+// console.log(Math.floor(((Math.random()*(max-(min-1))) + 1) + min-1))
+// console.log(Math.floor(((Math.random()*(max-(min))) + 1) + min))
+
+
+// console.log(Math.floor(((Math.random()*11) + 1) + 9))
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+console.log(Math.sqrt(25));
+console.log(Math.pow(5,2));
+console.log(Math.round(2.4));
+console.log(Math.floor(2.4));
+console.log(Math.ceil(2.4));
+console.log(Math.abs(-4));
+console.log(Math.min(1,2,6,4,3));
+console.log(Math.max(1,2,6,4,3));
+console.log(Math.random());
+
+
