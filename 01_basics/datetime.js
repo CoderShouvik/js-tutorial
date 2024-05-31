2510f142465513a29f0f212a08e9cee6692dc79e
Shouvik_Sarkar <87439972+CoderShouvik@users.noreply.github.com>
2024-05-31 06:34:01 +0000

    date and time

---
+++ 01_basics/datetime.js
@@ -0,0 +1,21 @@
+let myDate = new Date();
+
+console.log(myDate);
+console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+
+// let myCreatedDate = new Date(2023, 4, 24);
+// let myCreatedDate = new Date(2023, 4, 24, 5, 3);
+let myCreatedDate = new Date('2024-5-31');
+
+// console.log(myCreatedDate.toLocaleString());
+
+let newDate = Date.now();
+// console.log(newDate);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor((Date.now()/1000)))
+// console.log(typeof newDate)
