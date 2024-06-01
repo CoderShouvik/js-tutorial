0cb15beeb42b14bce98aec945d433bd5f39907ec
Shouvik_Sarkar <87439972+CoderShouvik@users.noreply.github.com>
2024-06-01 11:24:24 +0000

    objects destruture

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,12 @@
+//destructuring
+
+let myCourse = {
+    topic: 'React',
+    courseInstructor: 'Shouvik Sarkar',
+    price: 999
+}
+
+const {courseInstructor} = myCourse;
+
+console.log(courseInstructor);
+
