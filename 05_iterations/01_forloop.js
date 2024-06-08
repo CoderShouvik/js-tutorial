466c71ba55fe1df302916e1dd829ad1e23260fe2
Shouvik_Sarkar <87439972+CoderShouvik@users.noreply.github.com>
2024-06-08 07:16:24 +0000

    loops

---
+++ 05_iterations/01_forloop.js
@@ -0,0 +1,48 @@
+// for (let i = 0; i < 10; i++) {
+
+//     if (i==5) {
+//         console.log('5 is the best number');
+//     }
+//     console.log(i);
+    
+// }
+
+
+// for (let i = 0; i <=10; i++) {
+//     console.log(`Table of ${i}`);
+//     for (let j = 0; j <=10; j++) {
+//         console.log(`${i}*${j} = ${i*j}`);
+//     }
+    
+// }
+
+// for (let index = 1; index <= 20; index++) {
+//     console.log(index);
+//     if (index == 9) {
+//         break;
+//     }
+    
+    
+// }
+
+// let i = 0;
+
+// while(i<=20)
+// {
+//     console.log(i);
+//     i += 2;
+// }
+
+let myArray = ['flash', 'bat man', 'superman'];
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+
+let i = 0;
+while(i< myArray.length)
+    {
+        console.log(myArray[i]);
+        i++;
+    }
